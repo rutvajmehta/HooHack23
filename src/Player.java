bfc6d567db6756f64677f33dbdad2e9ea327c238
B K <bkhan26@GMU.EDU>
2023-03-26 00:23:54 -0400

    Player 1st

---
+++ src/Player.java
@@ -0,0 +1,37 @@
+public class Player {
+
+    //    private int points, assist, rebounds, steals, blocks;
+    private String name;
+    private Game [][] gamesStats = new Game[22][];
+    private int gameIndex =0;
+
+    public Player(String name, Game gameStat){
+        this.name = name;
+
+    }
+
+    public void setGameStats(String name, Game[] gameStats){
+        if(this.name == name) {
+            gamesStats[gameIndex] = new Game[gameStats.length];
+            gameIndex++;
+        }
+        else
+            System.out.println("Wrong Player");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Game[] [] getGamesStats() {
+//        for(int i = 0; i < gamesStats.length; i++){
+//            for(int j =0; j < gamesStats[i].length; j++){
+//                System.out.print(gamesStats[i][j]);
+//            }
+//            System.out.println();
+//        }
+        return gamesStats;
+    }
+
+
+}
